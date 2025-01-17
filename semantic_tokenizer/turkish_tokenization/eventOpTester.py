8bf31501c5f7e0ea1c4e47ef19dfae365fbb0278
malibayram <malibayram20@gmail.com>
2025-01-17 15:15:30 +0300

    ekler ve kokler için ilk versiyon liste eklendi

--- semantic_tokenizer/turkish_tokenization/eventOpTester.py
+++
@@ -1,7 +0,0 @@
-from eventOperator import EventOperator
-
-eventOp = EventOperator()
-
-# Test the consonantHardening function
-print(eventOp.consonantHardening("kitap", "cı"))
-print(eventOp.revertConsonantHardening("kitapçı", "kitap"))
